850fb546ec7b8f96ae44a187f44ce9f1ce8b9b0e
mickeysss <sargsyanmikael@mail.ru>
2022-04-17 01:09:07 +0300

    added structure for page 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import {Route, Routes } from 'react-router-dom';
 import './App.css';
+import {Header} from './components/Header';
+import {Main} from './pages/Main';
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
-      Auction-app
+
+      <Header/>
+        <Routes>
+            <Route path="/" element={<Main/>}/>
+        </Routes>
     </div>
   );
 }
 
-export default App;
