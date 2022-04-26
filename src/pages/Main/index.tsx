8344fe4cfb5425fb7a1ef1c1d0cb5a629faba9c7
mickeysss <sargsyanmikael@mail.ru>
2022-04-26 09:45:30 +0300

    added Main page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,3 +1,4 @@
+import CarouselComponent from 'components/Carousel';
 import React from 'react';
 import { carouselItems,carouselItemType } from '../../mocks/carouselMocks';
 
@@ -5,18 +6,8 @@ import styles from './styles.module.scss'
 
 export const Main = () => {
     return (
-        <div className={styles.mainContainer}>
-            {carouselItems.map((carouselItem: carouselItemType) => (
-                    <div className={styles.adsList}>
-                        <div key={carouselItem.id} className={styles.adsItem}>
-                            <h2>{carouselItem.title}</h2>
-                            <div>{carouselItem.category}</div>
-                            <h3>{carouselItem.itemName}</h3>
-                            <p>{carouselItem.description}</p>
-                            <img className={styles.adImage} src={carouselItem.image} alt="image-item"/>
-                        </div>
-                    </div>
-                ))}
+        <div className={styles.container}>
+            <CarouselComponent/>
         </div>
     );
 };
