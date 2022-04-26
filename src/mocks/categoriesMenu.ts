f82a911463c18bb2cbebcf7cfed9496a70406d4c
mickeysss <sargsyanmikael@mail.ru>
2022-04-26 09:42:56 +0300

    added pictures

---
+++ src/mocks/categoriesMenu.ts
@@ -0,0 +1,35 @@
+import clothesImg from '../assets/imgs/categories/clothes-1.jpg'
+import ownGoodsImg from '../assets/imgs/categories/ownGoods.jpg'
+import shoesImg from '../assets/imgs/categories/shoes.jpg'
+import electronicsImg from '../assets/imgs/categories/electronics.jpg'
+import accesoriesImg from '../assets/imgs/categories/accesories.jpg'
+
+
+
+export const categoriesMenu = [
+    {
+        id: 1,
+        title: 'Одежда',
+        img: clothesImg
+    },
+    {
+        id: 2,
+        title: 'Обувь и галантерея',
+        img: shoesImg
+    },
+    {
+        id: 3,
+        title: 'Личные вещи',
+        img: ownGoodsImg
+    },
+    {
+        id: 4,
+        title: 'Техника и товары для дома',
+        img: electronicsImg
+    },
+    {
+        id: 5,
+        title: 'Украшения и аксессуары',
+        img: accesoriesImg
+    },
+]
